059f8664c109757a5f2d378f3dc3e33993421c41
gusschool2 <57670825+gusschool2@users.noreply.github.com>
2019-11-13 16:36:01 +0300

    Update program.py
    GitHub Commit

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('New commit from PyCharm')
+    print('New commit from GitHub')
 
 
 if __name__ == '__main__':
